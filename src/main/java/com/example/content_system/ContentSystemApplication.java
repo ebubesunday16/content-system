a184d96b6ac9dd5841ad03513c5d90dfb39dc7c2
Emmanuel Sunday <135783723+ebubesunday16@users.noreply.github.com>
2026-02-10 19:32:45 +0100

    Initial commit

---
+++ src/main/java/com/example/content_system/ContentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.content_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentSystemApplication.class, args);
+	}
+
+}
